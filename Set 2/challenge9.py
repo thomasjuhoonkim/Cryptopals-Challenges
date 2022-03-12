b9c955d5fd2a9e3b641596b49f09736e9991c792
ThomasKim1010 <63864727+ThomasKim1010@users.noreply.github.com>
2022-03-12 01:34:29 -0500

    Challenge 9 Complete

---
+++ Set 2/challenge9.py
@@ -0,0 +1,14 @@
+import binascii
+
+def pkcs7_padding(plaintext, block_size = 16):
+    assert len(plaintext) < block_size, "Plaintext is longer than intended block size"
+    padding_length = block_size - len(plaintext)
+    padding_length_hex = hex(padding_length).lstrip("0x")
+    if len(padding_length_hex) % 2 != 0:
+        padding_length_hex = "0" + padding_length_hex
+    plaintext += binascii.unhexlify(padding_length_hex) * padding_length
+    return plaintext
+
+plaintext = bytes("YELLOW SUBMARINE", "utf8")
+block_size = 20
+print(pkcs7_padding(plaintext, block_size))
