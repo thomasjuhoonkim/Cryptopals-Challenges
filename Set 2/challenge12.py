5f9a4a8ad81665ec703e9d92e08979f795384149
ThomasKim1010 <thomasjuhoonkim@gmail.com>
2022-03-14 00:14:16 -0400

    Some Challenge 12 Progress

---
+++ Set 2/challenge12.py
@@ -0,0 +1,11 @@
+import set1
+from challenge11 import random_block_of_bytes
+
+
+ciphertext = """Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
+aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
+dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
+YnkK"""
+key = random_block_of_bytes(16)
+
+print(set1.base64_to_hex(ciphertext))
